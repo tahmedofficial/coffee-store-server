0b0211e55462b17239eb78edf03483168edfacdd
tahmedofficial <tanvirofcuo@gmail.com>
2024-05-09 14:34:04 +0600

    added token for apis

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 app.use(express.json());
-app.use (cors({origin:["localhost url","https://coffee-store-server-umber-five.vercel.app"]}))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,6 +20,12 @@ const client = new MongoClient(uri, {
     }
 });
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -29,6 +35,21 @@ async function run() {
         const database = client.db("coffeeDb");
         const coffeeCollection = database.collection("coffee");
         const userCollection = database.collection("user");
+
+        // require("crypto").randomBytes(64).toString("hex")
+
+        // Auth related apis
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: "1h" })
+            res.cookie("token", token, cookieOptions).send({ success: true })
+        })
+
+        app.post("/logout", async (req, res) => {
+            res.clearCookie("token", { maxAge: 0 }).send({ success: true })
+        })
+
+
 
         // coffee related Apis
         app.get("/coffee", async (req, res) => {
@@ -119,7 +140,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
